2472e9cc3008b4cf820089ab796c09f8c8c72e99
Alfie Pates <alfie@alfiepates.me>
2017-09-13 19:30:15 +0100

    added test for some planned/unimplemented features

--- spec.rb
+++ spec.rb
@@ -25,4 +25,12 @@ RSpec.describe "./borkstrap" do
 			expect(`./borkstrap --help`).to include "--repo"
 		end
 	end
+
+	# Unimplemented Features
+	it "checks operating system"
+	it "installs homebrew on MacOS"
+	it "installs linuxbrew on Linux"
+	it "installs bork"
+	it "pulls a git respository if specified"
+	it "executes bork on the git repository if specified"
 end
