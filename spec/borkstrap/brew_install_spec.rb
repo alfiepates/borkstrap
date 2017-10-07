d13da76070a50c9fa1cec607e0dc73c02b87bc70
Alyssa Ross <hi@alyssa.is>
2017-10-07 15:35:40 +0100

    tests for downloading brew install script

--- spec/borkstrap/brew_install_spec.rb
+++ spec/borkstrap/brew_install_spec.rb
@@ -8,14 +8,17 @@ RSpec.describe Borkstrap::BrewInstall do
 	describe "#install_script" do
 		subject { described_class.new(install_script_url: "https://example.com") }
 
-		before do
-		  # YAU SET THE TAB WIDTH FOR ME <333
+		let!(:request_stub) do
 		  stub_request(:get, "https://example.com").to_return(body: "foo bar")
 		end
-	
+
 		it "fetches the install script from GitHub" do
-			expect(subject.install_script.read).to eq "foo bar"
+			expect(File.read(subject.install_script)).to eq "foo bar"
 		end
-		it "does not re-fetch the install script"
+
+		it "does not re-fetch the install script" do
+		  2.times { subject.install_script }
+		  expect(request_stub).to have_been_requested.once
+		end
 	end
 end
