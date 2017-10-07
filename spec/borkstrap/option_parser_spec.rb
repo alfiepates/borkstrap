86858f74c15768f1c9e38500bb1ba7a6af133ab4
Alyssa Ross <hi@alyssa.is>
2017-10-07 22:44:07 +0100

    specify --bork-HEAD option

--- spec/borkstrap/option_parser_spec.rb
+++ spec/borkstrap/option_parser_spec.rb
@@ -12,4 +12,26 @@ RSpec.describe Borkstrap::OptionParser do
 			expect(parser.call).to include(repo: "https://git.alfiepates.me/alfie/borkstrap")
 		end
 	end
+
+	describe "--bork-HEAD" do
+		it "sets :head to false if not supplied" do
+			parser = described_class.new([])
+			expect(parser.call).to include(bork_head: false)
+		end
+
+		it "sets :head to true if supplied" do
+			parser = described_class.new(%w[--bork-HEAD])
+			expect(parser.call).to include(bork_head: true)
+		end
+
+		it "can be overridden with --no-bork-HEAD" do
+			parser = described_class.new(%w[--bork-HEAD --no-bork-HEAD])
+			expect(parser.call).to include(bork_head: false)
+		end
+
+		it "can be reoverridden with --bork-HEAD" do
+			parser = described_class.new(%w[--bork-HEAD --no-bork-HEAD --bork-HEAD])
+			expect(parser.call).to include(bork_head: true)
+		end
+	end
 end
