7bc6ff77d2cdac4ca955dc1c24291ffdaafc9c63
Alyssa Ross <hi@alyssa.is>
2017-10-07 16:17:38 +0100

    simplify BrewInstall interface

--- spec/borkstrap/brew_install_spec.rb
+++ spec/borkstrap/brew_install_spec.rb
@@ -5,20 +5,18 @@ $VERBOSE = old_verbose
 require "spec_helper"
 
 RSpec.describe Borkstrap::BrewInstall do
-	describe "#install_script" do
-		subject { described_class.new(install_script_url: "https://example.com") }
+	subject { described_class.new(install_script_url: "https://example.com") }
 
-		let!(:request_stub) do
-		  stub_request(:get, "https://example.com").to_return(body: "foo bar")
-		end
+	describe "#install" do
+		it "executes the script" do
+			file = Tempfile.new(self.class.name)
 
-		it "fetches the install script from GitHub" do
-			expect(File.read(subject.install_script)).to eq "foo bar"
-		end
+		  stub_request(:get, "https://example.com").to_return(body: <<-RUBY)
+				File.write(#{file.path.inspect}, "hello")
+			RUBY
 
-		it "does not re-fetch the install script" do
-		  2.times { subject.install_script }
-		  expect(request_stub).to have_been_requested.once
+			subject.install
+			expect(file.read).to eq "hello"
 		end
 	end
 end
